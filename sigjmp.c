0e17069c4a0f075d8b71d5bac2d9f36ef7eada09
venture8343 <park0122@hanmail.net>
2017-12-28 13:12:51 +0900

    Update sigjmp.c
    Add comment

--- sigjmp.c
+++ sigjmp.c
@@ -1,3 +1,12 @@
+/* The sigsetjmp() function saves the current stack environment and, 
+   optionally, the current signal mask. The stack environment and 
+   signal mask saved by sigsetjmp() can subsequently be restored by siglongjmp().
+
+   sigsetjmp() is similar to setjmp(), except for the optional capability of
+   saving the signal mask. Like setjmp() and longjmp(), the sigsetjmp() and
+   siglongjmp() functions provide a way to perform a nonlocal "goto."
+*/
+
 #include <setjmp.h>
 #include <signal.h>
 #include <stdio.h>
@@ -20,13 +29,11 @@ int main(void)  {
        (sigaction(SIGINT, &act, NULL) == -1)) {
       perror("Failed to set up SIGINT handler");
       return 1; 
-   }
-                                                  /* stuff goes here */
+   }/* stuff goes here */
    fprintf(stderr, "This is process %ld\n", (long)getpid());
    if (sigsetjmp(jmpbuf, 1))
       fprintf(stderr, "Returned to main loop due to ^c\n");
    jumpok = 1;
    printf("test\n");
-   for ( ; ; ) 
-      ;                                       /* main loop goes here */
+   for ( ; ; );          /* main loop goes here */
 }
