2fa9d52c21fe90a07d3ba3deaa9f63d150d0dc68
venture8343 <park0122@hanmail.net>
2017-12-28 12:33:58 +0900

    주석 추가

--- setjmp.c
+++ setjmp.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <setjmp.h>
 
+// 실행시  "second"가 출력된 뒤
+// "main"을 출력하고 프로그램이 종료 된다. 
+// setjmp()는  현재 상태를 스택에 저장한 뒤
+// longjmp()호출시 스택에 저장된 값을 꺼내서
+// if(!setjmp(buf))위치로 이동한다. 
+ 
 static jmp_buf buf;
 
 void second(void) {
